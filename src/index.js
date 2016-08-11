8d8d3187d273f8830717a9d6395e043091037b4d
Jollen Chen <jollen@moko365.com>
2016-08-11 21:32:24 +0800

    minor update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 'use strict';
 
-var _AppDispatcher = require('./Dispatchers/AppDispatcher');
+var _react = require('react');
 
-var _AppDispatcher2 = _interopRequireDefault(_AppDispatcher);
+var _react2 = _interopRequireDefault(_react);
 
-var _AppStore = require('./Stores/AppStore');
-
-var _AppStore2 = _interopRequireDefault(_AppStore);
-
-var _AppActions = require('./Actions/AppActions');
+var _reactDom = require('react-dom');
 
 var _WebsocketView = require('./Components/WebsocketView');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = {
-	AppDispatcher: _AppDispatcher2.default,
-	AppStore: _AppStore2.default,
-	AppActions: _AppActions.AppActions,
-
 	WebsocketView: _WebsocketView.WebsocketView
 };
 //# sourceMappingURL=index.js.map
